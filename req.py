6fa3f087ec162b5870ae68fa34fc171768414fd6
Ryan Pedersen <ryanpedersen42@gmail.com>
2023-09-19 21:05:50 -0400

    add creds

--- req.py
+++ req.py
@@ -5,6 +5,7 @@ import json
 # These are the credentials passed by the 'port' context to your environment variables
 CLIENT_ID = os.environ['PORT_CLIENT_ID']
 CLIENT_SECRET = os.environ['PORT_CLIENT_SECRET']
+PR_REQUESTS_OPEN = os.environ['PR_REQUESTS_OPEN']
 
 credentials = {
     'clientId': CLIENT_ID,
@@ -20,7 +21,7 @@ headers = {
 entity_json = {
         "identifier": "ex2",
         "properties": {
-          "pull_requests_open": 14,
+          "pull_requests_open": PR_REQUESTS_OPEN,
       }
 }
 
